cd68cf0e392e5f2086cab1285a59e47591ad629e
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-30 23:42:42 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,88 @@
+export default {
+   personaleInfo:{
+    name: 'Bassel',
+    surename : 'ASSI',
+    email:'bassel-assi@live.com',
+    cellphone: 3201538330,
+    address: 'trieste'
+   },
+   profile:'Sono uno studente di magistrale in ingegneria di processo e materiali, con una grande passione e forte motivazione per diventare un sviluppatore fronte-end.'+
+            'competenza in javascript, css,html, react, Bootstrap e object-oriented-programing. Sono una'+
+            'persona seria con una grande capacità di adattamento e la voglia di imparare. Cerco la mia prima esperienza per fare partire la mia carriera.',
+   studies: [
+        {
+            degree:'Laurea triennale in ingegneria industriale',
+            schoole:'Università degli studi di Trieste',
+            start:'2012',
+            end:'2016'
+        },
+        {
+            degree:'Corso magistrale in ingegneria dei processi e dei materiali',
+            schoole:'Università degli studi di Trieste',
+            start:'2017',
+            end:'2023'
+        }
+   ],
+   exprience:[
+        {
+            postion:'Porta pizza',
+            company:'PIZZA PARINI',
+            start:'2022',
+            end:'oggi'
+        },
+        {
+            postion:'Cameriere',
+            company:'HOSTARIA MALCANTON',
+            start:'2021',
+            end:'2022'
+        },
+        {
+            postion:'Venditore d’ auto',
+            company:'REAUTO',
+            start:'2021',
+            end:'2021'
+        }
+   ],
+   skills: [
+        {
+            skill:'JAVASCRIPT',
+            competence: 75
+        },
+        {
+            skill:'CSS',
+            competense: 60
+        },
+        {
+            skill:'HTML',
+            competense: 75
+        },
+        {
+            skill: 'REACT',
+            competense: 60
+        },
+        {
+            skill:'BOOTSTRAP',
+            competense: 30
+        },
+        {
+            skill:'SQL',
+            competense: 20
+        },
+    
+   ],
+   languages : [
+    {
+        language:'ITALIANO',
+        level: 90
+    },
+    {
+        language:'INGLESE',
+        level: 90
+    },
+    {
+        language: 'ARABO',
+        level:100
+    }
+   ]
+   
+}
