e0913f81d0c855351bfcc7c427df0e5fed7b4051
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-07-01 22:41:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/nabar';
+import Resume from './components/resume'
+import CreateResume from "./components/creatResume";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <Navbar/>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element={<Resume/>}/>
+        <Route path='/createReume' element={<CreateResume/>}/>
+      </Routes>
+    </BrowserRouter>
+      
     </>
   );
 }
