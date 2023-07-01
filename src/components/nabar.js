ae5b35ad1ab4436d72396787b0a0f908e787e6ad
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-07-01 22:41:10 +0200

    Update nabar.js

--- src/components/nabar.js
+++ src/components/nabar.js
@@ -1,17 +1,22 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { useNavigate } from "react-router-dom";
+
 import './navbar.css'
 import { Button } from "bootstrap";
 export default function Navbar(){
 
-
+    const navigate = useNavigate()
+    function changeRoute(){
+        navigate('/')
+    }
     return(
        <nav className="navbar navbar-expand">
             <div className="container w-100">
                 <ul className="navbar-nav w-100">
                     <li className="nav-item"><button className="nav-link">ABOUT ME</button></li>
-                    <li className="nav-item"><button className="nav-link">RESUME</button></li>
+                    <li className="nav-item"><button className="nav-link" onClick={changeRoute}>RESUME</button></li>
                     <li className="nav-item"><button className="nav-link">PROJECTS</button></li>
                 </ul>
             </div>
