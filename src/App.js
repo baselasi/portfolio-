72cf3fe5558c7330507772fab64220cec10fc69b
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-08-19 22:26:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import Navbar from './components/nabar';
 import Resume from './components/resume'
 import CreateResume from "./components/creatResume";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Sum from './components/sum';
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
+    <Sum/>
+      {/*<Routes>
         <Route path='/' element={<Resume/>}/>
         <Route path='/createReume' element={<CreateResume/>}/>
-      </Routes>
-    </BrowserRouter>
+      </Routes>*/}
       
     </>
   );
