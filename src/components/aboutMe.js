fc60cc166e5e2ae48c4bd49da987f5295c7b41f4
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-08-20 23:25:41 +0200

    Update aboutMe.js

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,11 +1,18 @@
 import React from "react";
 import background1 from "../imgs/img.jpg"
 import Navbar from "./nabar";
-
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./sum.css"
+import data from "../data";
 export default function AboutMe(){
     return(
-        <div style={{backgroundImage : `url(${background1})`}}>
-            <div style={{color: "white"}}>FRONT-END DEVOLEOPER / PROCESS AND MATERIAL ENGINEER</div>
-        </div>
+        <section  className=" section  row align-content-center">
+            <img src="/imgs/img.jpg" className="imgs"></img>
+            
+            <div className="first-continer col-sm-6 ">
+                <div className="h2 ">HI, I'M <span className=" danger">BASSEl ASSI</span>, FRONT-END DEVOLEOPER / PROCESS AND MATERIAL ENGINEER</div>
+                <div className="h4 container">{data.aboutMe.profile}</div>
+            </div>
+        </section>
     )
 }
