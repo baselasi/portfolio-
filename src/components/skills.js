841b6c13c23e3c8d0c27ae0328845d3ad0ede67e
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-08-22 23:01:56 +0200

    Update skills.js

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data";
 import "./skill.css"
+import ScrollTrigger from "react-scroll-trigger";
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
 
 export default function Skills(){
+    const [firstWidth,setWidth] = useState([])
+    const [secondWidth,setSecondwidth]=useState([])
+    function changeWidth(skill,skillSet){
+        switch(skillSet)
+        {
+            case "programing":
+                setWidth((prev)=>[...prev,skill.competence])
+                break;
+            case "enginering":
+                setSecondwidth((prev)=>[...prev,skill.competence])
+                break;
+            default:
+                console.log("as")
+        }
+       
+    }
     return(
+        <ScrollContainer>
         <section className="h-100">
             <div className=" container">
-                <h2 className="border-bottom">PROGRAMING SKILLS</h2>
-                <div className=" row">
-                    {data.programnigSkills.map((skill)=>{
+                
+                    <Animator animation={batch( Move(500,0))}>
+                        <h2 className="border-bottom w-25 text-center">PROGRAMING SKILLS</h2>
+                    </Animator>
+                    
+                    <div className=" row">
+                        {data.programnigSkills.map((skill)=>{
                         return(
                             <div className="  col-md-4 d-flex flex-column justify-content-center align-items-center">
                                 <img className="logo " src={skill.logo}></img>
                                 <h4>
                                 {skill.skill}
                                 </h4>
-                                <div className="continer-skill"><div  style={{width:`${skill.competense}%`}} className="skill-level"></div>{skill.competense}%</div>
+                                <ScrollTrigger onEnter={()=>changeWidth(skill,"programing")} style={{width:"100%"}}><div className="continer-skill"><div  style={{width:`${firstWidth[data.programnigSkills.indexOf(skill)]}%`}} className="skill-level"></div>{skill.competence}%</div></ScrollTrigger>
                             </div>
                         )
                     })}
-                </div>
+                    </div>
+                
+             
             </div>
             <div className="container">
-                <h2 className="border-bottom">ENGINERING SKILLS</h2>
+            <Animator animation={batch( Move(500,0))}>
+                <h2 className="border-bottom w-25 text-center">ENGINERING SKILLS</h2>
+                </Animator>
                 <div className=" row">
                     {data.engineeringSkills.map((skill)=>{
                         return(
@@ -31,20 +58,21 @@ export default function Skills(){
                                 <h4>
                                 {skill.skill}
                                 </h4>
-                                <div className="continer-skill"><div  style={{width:`${skill.competence}%`}} className="skill-level"></div>{skill.competence}%</div>
+                                <ScrollTrigger onEnter={()=>changeWidth(skill,"enginering")} style={{width:"100%"}}><div className="continer-skill"><div  style={{width:`${secondWidth[data.engineeringSkills.indexOf(skill)]}%`}} className="skill-level"></div>{skill.competence}%</div></ScrollTrigger>
+                                
                             </div>
                         )
                     })}
                 </div>
             </div>
             <div className="container">
-                <h2 className="border-bottom">SOFT SKILLS</h2>
+                <h2 className="border-bottom w-25 text-center">SOFT SKILLS</h2>
                 <div className=" row">
                     {data.softSkills.map((skill)=>{
                         return(
                             <div className="  col-md-4 d-flex flex-column justify-content-center align-items-center">
-                                 <img className="logo " src={skill.logo}></img>
-                                <h4>
+                                <img className="logo " src={skill.logo}></img>
+                                <h4 >
                                 {skill.skill}
                                 </h4>
                             </div>
@@ -55,7 +83,9 @@ export default function Skills(){
             <div>
 
             </div>
+            
+
         </section>
-        
+        </ScrollContainer>
     )
 }
