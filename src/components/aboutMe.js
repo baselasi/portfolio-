808fbc2b5240a2ac0c21a2f305f823fd49439c85
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-15 22:48:46 +0200

    Update aboutMe.js

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -4,10 +4,10 @@ import "./sum.css"
 import data from "../data";
 export default function AboutMe(){
     return(
-        <section  className=" section  row align-content-center">
-            <img src="/imgs/img.jpg" className="imgs"></img>
+    
+    <section  className=" section  row align-content-center sticky" style={{zIndex:"-1",backgroundColor:"black"}}>
             
-            <div className="first-continer col-sm-6 ">
+            <div className="first-continer col-sm-12 ">
                 <div className="h2 ">HI, I'M <span className=" danger">BASSEl ASSI</span>, FRONT-END DEVOLEOPER / PROCESS AND MATERIAL ENGINEER</div>
                 <div className="h4 container">{data.aboutMe.profile}</div>
             </div>
