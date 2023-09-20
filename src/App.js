2fd8f1ac89044e0dcd7bf6234efa90555ad4b5f2
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-20 23:06:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-
-import Sum from './components/sum';
+import AboutMe from './components/aboutMe';
+import About from './components/about';
+import Navbar from './components/nabar';
 import Skills from './components/skills';
+import ContactMe from './components/contactMe';
+import { useState } from 'react';
 function App() {
+  const [showButton,setButton] = useState(true)
   return (
     
-    <Sum/>
+    <>
+      <AboutMe 
+        showButton={showButton}
+      />
+      <Navbar 
+        setButtonOpacity={setButton}
+      />
+      <About/>
+      <Skills/>
+      <ContactMe/>
+    </>
     
       
     
