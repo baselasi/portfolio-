8c091001431c00ddb9daa9e4bfa944d7715891cd
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-20 23:06:44 +0200

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -4,11 +4,15 @@ import data from "../data";
 
 export default function About(){
     return(
-        <section   style={{backgroundColor:"rgb(255, 255, 255)",color:"rgb(20, 33, 61)",height:"50vh"}}>
-            <h2>ABOUT</h2>
-            <p>
-                {data.profile}
-            </p>
+        <section className="row"  style={{backgroundColor:"rgb(255, 255, 255)",color:"rgb(20, 33, 61)"}}>
+            <h2 className="col-sm-4">WHO <br></br>AM I?</h2>
+            <div className="col-sm-8" >
+                <p style={{fontSize:"30px"}}>
+                    {data.profile}
+                </p>
+                <button><a href="CV Bassel-Assi.pdf" download="cv">download</a></button>
+            </div>
+            
         </section>
     )
 }
