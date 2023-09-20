6c3206ac15f0a1106a96b4617390fadde4a0ae02
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-20 23:06:57 +0200

    Update nabar.js

--- src/components/nabar.js
+++ src/components/nabar.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { useNavigate } from "react-router-dom";
 
 import './navbar.css'
 import { Button } from "bootstrap";
-export default function Navbar(){
 
+export default function Navbar(props){
+    const [isShowing,setShow] = useState(false)
+    const ref=useRef()
+    useEffect(()=>{
+        const observer = new IntersectionObserver((entries)=>{
+            entries.forEach((entrie)=>{
+                    props.setButtonOpacity([entrie.isIntersecting])
+                    setShow(entrie.isIntersecting)
+            })
+        })
+        observer.observe(ref.current)
+    },[])
     //const navigate = useNavigate()
-    function changeRoute(){
-        navigate('/')
-    }
     return(
-       <nav className="navbar navbar-expand sticky nav-bar " style={{backgroundColor:"rgb(71, 75, 79)",top:"0.1px" ,zIndex:"2"}}>
-            <div className="container w-100">
-                <ul className="navbar-nav w-100">
-                    <li className="nav-item"><button className="nav-link">ABOUT ME</button></li>
-                    <li className="nav-item"><button className="nav-link" >RESUME</button></li>
-                    <li className="nav-item"><button className="nav-link">PROJECTS</button></li>
+       <nav  className="navbar navbar-expand sticky nav-bar " style={{opacity:isShowing?"0.9":"1",zIndex:"2"}}>
+            <div ref={ref} className="container w-100" >
+                <ul className="navbar-nav w-100" >
+                    <li className="nav-item"><button className="nav-link" style={{color:"rgb(102, 255, 0)"}}>ABOUT ME</button></li>
+                    <li className="nav-item"><button className="nav-link" style={{color:"rgb(102, 255, 0)"}}>RESUME</button></li>
+                    <li className="nav-item"><button className="nav-link" style={{color:"rgb(102, 255, 0)"}}>PROJECTS</button></li>
                 </ul>
             </div>
        </nav>
