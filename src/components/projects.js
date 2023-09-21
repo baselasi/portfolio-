6f10edaba935f1288e417d2f6a27570f02f442d0
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-21 22:07:44 +0200

    Create projects.js

---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+import data from "../projectsData"
+export default function Projects(){
+    
+    class projects{
+        constructor(){
+            this.i="w"
+        }
+    }
+
+    o=new projects
+    console.log(o)
+}
