9aba316b2406af0fe72fe184c6452a209ddb5070
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-22 22:56:16 +0200

    Create projectsData.js

---
+++ src/projectsData.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        title: "wather app",
+        discription:"",
+        imgs:["../imgs/projectsImgs/watherApp0.PNG","../imgs/projectsImgs/watherApp1.jpeg","../imgs/projectsImgs/watherApp2.jpeg","../imgs/projectsImgs/watherApp3.PNG","../imgs/projectsImgs/watherApp4.PNG"]
+    },
+    {
+        title:"countries list",
+        discription:"",
+        imgs:["../imgs/projectsImgs/contries0.PNG","../imgs/projectsImgs/contries1.jpeg","../imgs/projectsImgs/contries2.jpeg","../imgs/projectsImgs/contries3.jpeg","../imgs/projectsImgs/contries4.jpeg"]
+    },
+    {
+        title:"E-comerce filter",
+        discription:"",
+        imgs:["../imgs/projectsImgs/filter0.PNG","../imgs/projectsImgs/filter1.PNG","../imgs/projectsImgs/filter2.PNG","../imgs/projectsImgs/filter3.PNG"]
+    },
+    {
+        title:"ecommerce product's page",
+        discription:"",
+        imgs:["../imgs/projectsImgs/ecommerce-product0.PNG","../imgs/projectsImgs/ecommerce-product1.jpeg","../imgs/projectsImgs/ecommerce-product2.jpeg","../imgs/projectsImgs/ecommerce-product3.jpeg","../imgs/projectsImgs/ecommerce-product4.jpeg"],
+
+    }
+]
