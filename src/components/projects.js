223f54adf367e98b5cffa9041fffc1c8796d1d03
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-22 22:56:13 +0200

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import data from "../projectsData"
+import "./projects.css"
 export default function Projects(){
-    
-    class projects{
-        constructor(){
-            this.i="w"
+    const [divs,setDivs]=useState([])
+    const [currentPosition,setPosition] = useState(0)
+    useEffect(()=>{
+        setDivs(projects)
+
+    },[])
+    function renderProjects(obj){
+        return(
+            <div ref={(el)=>projects[data.indexOf(obj)]=el} className="cover" style={{backgroundImage:`url(${obj.imgs[0]})`}}></div>
+        )
+    }
+    let projects= useRef(new Array())
+    if( divs[currentPosition]){
+        divs[currentPosition].scrollIntoView({ behavior: "smooth" })
+    }
+    function scrollRight(){
+        if(currentPosition<3){
+            setPosition((prev)=>prev+1)
+        }
+        else{
+
         }
     }
+    function scrollleft(){
+        if(currentPosition>0){
+            setPosition((prev)=>prev-1)
+        }
+    }
+    return(
+        <section style={{backgroundColor:"rgb(248, 246, 244)"}} className=" marging-padding border-bottom border-5 border-primary">
 
-    o=new projects
-    console.log(o)
+            <button style={{position:"absolute"}} onClick={scrollRight}>+</button>
+            <div className=" projects">
+            { 
+               data.map((obj)=>{
+                return(
+                    <div className=" marging-padding flex " >
+                    {
+                        renderProjects(obj)
+                    }
+                    </div>
+               
+                )
+               })
+            }
+            </div>
+            
+            <button style={{position:"absolute",right:"50%"}} onClick={scrollleft}>-</button>
+        </section>            
+    )
 }
