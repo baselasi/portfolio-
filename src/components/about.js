2570d0cdb082a6829145ee134fa241e37291d613
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-23 23:18:46 +0200

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -6,9 +6,9 @@ export default function About(){
     return(
         <section className="flex justify-content-center marging-padding border-bottom border-5 border-primary"   style={{backgroundColor:"rgb(248, 246, 244)",color:"rgb(20, 33, 61)",marginTop:"0"}}>
            <div className="row col-12">
-           <h2 className="col-sm-3" style={{fontSize:"45px"}}>WHO AM I?</h2>
+           <h2 className="col-sm-3" style={{fontSize:"45px",color:"green"}}>WHO AM I?</h2>
             <div className="col-sm-9" >
-                <p style={{fontSize:"30px"}}>
+                <p style={{fontSize:"30px",color:"rgb(102, 255, 0)"}}>
                     {data.profile}
                 </p>
                 <button className="col-3 btn "><a href="CVBassel-Assi.pdf" download="ASSI-BASSEL RESUME" style={{width:"100%"}}>DOWNLOAD RESUME </a></button>
