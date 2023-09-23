d323499595ba8fa452ea13a7e5f36a2bdd0f1bf2
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-23 23:18:33 +0200

    Update projectsData.js

--- src/projectsData.js
+++ src/projectsData.js
@@ -2,21 +2,29 @@ export default [
     {
         title: "wather app",
         discription:"",
+        show: false,
         imgs:["../imgs/projectsImgs/watherApp0.PNG","../imgs/projectsImgs/watherApp1.jpeg","../imgs/projectsImgs/watherApp2.jpeg","../imgs/projectsImgs/watherApp3.PNG","../imgs/projectsImgs/watherApp4.PNG"]
     },
     {
         title:"countries list",
-        discription:"",
+        discription:" Users should be able to: 1.See all countries from the API on the homepage"+
+       " Search for a country using an input field"+
+       " Filter countries by region"+
+        "Click on a country to see more detailed information on a separate page+"+
+       " Click through to the border countries on the detail page"+
+        "Toggle the color scheme between light and dark mode (optional)",
         imgs:["../imgs/projectsImgs/contries0.PNG","../imgs/projectsImgs/contries1.jpeg","../imgs/projectsImgs/contries2.jpeg","../imgs/projectsImgs/contries3.jpeg","../imgs/projectsImgs/contries4.jpeg"]
     },
     {
         title:"E-comerce filter",
         discription:"",
+        show: false,
         imgs:["../imgs/projectsImgs/filter0.PNG","../imgs/projectsImgs/filter1.PNG","../imgs/projectsImgs/filter2.PNG","../imgs/projectsImgs/filter3.PNG"]
     },
     {
         title:"ecommerce product's page",
         discription:"",
+        show: false,
         imgs:["../imgs/projectsImgs/ecommerce-product0.PNG","../imgs/projectsImgs/ecommerce-product1.jpeg","../imgs/projectsImgs/ecommerce-product2.jpeg","../imgs/projectsImgs/ecommerce-product3.jpeg","../imgs/projectsImgs/ecommerce-product4.jpeg"],
 
     }
