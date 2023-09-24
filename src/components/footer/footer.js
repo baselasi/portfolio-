148d8a1017b2baf0ada052ea24cff5089466ac40
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-09-24 22:53:58 +0200

    Create footer.js

---
+++ src/components/footer/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./footer.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee ,faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+export default function Footer(){
+
+
+
+    return(
+        <footer style={{backgroundColor:"rgb(248, 246, 244)"}} className="footer ">
+            <div className="icons">
+                <a href="mailto:bassel-assi@live.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href="https://www.linkedin.com/in/basselassi/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                <a href="https://github.com/bassel-assi"><FontAwesomeIcon icon={faGithub} /></a>
+            </div>
+            <div>
+                BASSEL ASSI
+            </div>
+        </footer>    
+    )
+}
