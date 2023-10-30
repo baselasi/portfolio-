9971b59da49f0cc99f476df4f404ba6c53266d8a
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-10-30 21:56:07 +0100

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.css"
+import "../style.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee ,faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -11,9 +12,9 @@ export default function Footer(){
     return(
         <footer style={{backgroundColor:"rgb(248, 246, 244)"}} className="footer ">
             <div className="icons">
-                <a href="mailto:bassel-assi@live.com"><FontAwesomeIcon icon={faEnvelope} /></a>
-                <a href="https://www.linkedin.com/in/basselassi/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-                <a href="https://github.com/bassel-assi"><FontAwesomeIcon icon={faGithub} /></a>
+                <a href="mailto:bassel-assi@live.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href="https://www.linkedin.com/in/basselassi/" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                <a href="https://github.com/bassel-assi" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
             </div>
             <div>
                 BASSEL ASSI
