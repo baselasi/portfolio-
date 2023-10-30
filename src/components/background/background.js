b32f659f55c57a0848e7436469596fb76ffd3eba
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-10-30 21:56:10 +0100

    Update background.js

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -82,22 +82,20 @@ export default function AboutMe(props){
     return(
     <>
         <div className="empty-div" style={{left:"0%"}} onMouseMove={change}></div>
-       
         <div className="empty-div flex-column align-items-center" style={{width:"50%",left:"25%",justifyContent:"flex-end",display:"flex"}}onMouseEnter={bringback}>
-            <button style={{opacity:dinamicaIntro ? "0" : "1",transition:"opacity ease-out 1s", backgroundColor:"rgba(155, 189, 168, 0.568)" }} className="btn" onClick={scrollIntoContact}>
+            <button style={{display:dinamicaIntro ? "none" : "block",transition:"opacity ease-out 1s", backgroundColor:"rgba(155, 189, 168, 0.568)" }} className="btn" onClick={scrollIntoContact}>
                 HIT ME UP
             </button>
         </div>
         <div className="empty-div" style={{left:"75%"}} onMouseMove={change}></div>
-        
-        <div  className=" section  row align-content-center sticky" style={{zIndex:"-1",color:"rgb(15, 98, 146)"}} >
+        <div  className=" section  row align-content-center sticky gx-0"  style={{zIndex:"-1",color:"rgb(15, 98, 146)"}} >
             <div   className="lists" style={{backgroundColor:"black"}}>
                     {renderCntent()}
             </div>
-            <div className="col-sm-6 intro">
+            <div className="col-sm-6 intro" style={{fontFamily:dinamicaIntro ? 'Binary X CHR BRK':"Georgia, 'Times New Roman', Times, serif"}}>
                 <h1 className=" h-25 flex-box-column title" style={{color: !dinamicaIntro ? "rgb(232, 249, 253)":"green"}}>
                     <span>{!dinamicaIntro ? "Bassel Assi": "01101000  01101100 01101111"}</span>
-                    <br></br> {!dinamicaIntro ? "Frontend Devoloper / Process And Material Engineer": "01101000  01101100 01101111 01101000 01101111"} </h1>
+                    <br></br> {!dinamicaIntro ? "Frontend Developer / Process And Material Engineer": "01101000  01101100 01101111 01101000 01101111"} </h1>
                 <div></div>
             </div>
         </div>
