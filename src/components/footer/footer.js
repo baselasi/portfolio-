2672aa1d3be30f4d49d729651246b281e24770e2
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-12-08 19:21:53 +0100

    m

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -14,7 +14,7 @@ export default function Footer(){
             <div className="icons">
                 <a href="mailto:bassel-assi@live.com" target="_blank"><FontAwesomeIcon icon={faEnvelope} /></a>
                 <a href="https://www.linkedin.com/in/basselassi/" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-                <a href="https://github.com/bassel-assi" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+                <a href="https://github.com/baselasi" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
             </div>
             <div>
                 BASSEL ASSI
