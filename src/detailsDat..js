5b1362c1bd3c77b2c05605559aa4a2ccfaf0e999
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-12-17 20:44:20 +0100

    Update detailsDat..js

--- src/detailsDat..js
+++ src/detailsDat..js
@@ -1,19 +1,23 @@
 export default {
-    eduction : [
+    eduction: [
         {
-            titlte:"self eduction",
-            description:"when it come to programming i mainly taught myself how to program, i started from,"+
-            "with html css and javascript, first by reading documentation and jumping straight into programing"+
-            "simple front-end components or arcades games, "+
-            "after a while i start learning react and bootstrap to complete my programmer profile programming some projects on the way."
-    },
-    {
-        titlte:"web development course",
-        description:"Intensive Front-End Development , duration 200 hours in presence , in Riendal a software company based in Reggio emilia"
-    },
-    {
-        titlte:"bechlore degree",
-        description:""
-    }
+            title: "self eduction",
+            description: "when it come to programming i mainly taught myself how to program, i started from," +
+                "with html css and javascript, first by reading documentation and jumping straight into programing" +
+                "simple front-end components or arcades games, " +
+                "after a while i start learning react and bootstrap to complete my programmer profile programming some projects on the way."
+        },
+        {
+            title: "web development course",
+            description: "Intensive Front-End Development , duration 200 hours in presence , in Riendal a software company based in Reggio emilia"
+        },
+        {
+            title: "bechlore degree",
+            description: "some more things to say"
+        },
+        {
+            title: "master degree",
+            description: "IN PROGRESS"
+        }
     ]
 }
