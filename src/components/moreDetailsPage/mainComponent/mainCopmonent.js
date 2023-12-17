7a7866563457552186f7c64f2204ddc0e71ce55d
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-12-17 20:44:28 +0100

    Update mainCopmonent.js

--- src/components/moreDetailsPage/mainComponent/mainCopmonent.js
+++ src/components/moreDetailsPage/mainComponent/mainCopmonent.js
@@ -1,11 +1,12 @@
 import React from "react";
 import LeftSidbar from "../sideWaySideBar/sideWayNavBar";
+import Eduction from "../eduction/eduction";
 export default function MoreDetailsMain(){
     return(
-       <main style={{backgroundColor:"black"}}>
+       <main style={{backgroundColor:"black",color:"green",fontFamily:"'SF Pixelate', sans-serif"}}  className="row">
         {/* <div>COMING SOON</div> */}
-
         <LeftSidbar/>
+        <Eduction/>
        </main>
         
     )
