776cf5d8ef28b1c1cbc3eca3a7f42c382d04a716
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-12-17 20:44:31 +0100

    Update eduction.js

--- src/components/moreDetailsPage/eduction/eduction.js
+++ src/components/moreDetailsPage/eduction/eduction.js
@@ -1,9 +1,23 @@
 import React from "react";
+import detailsDat from "../../../detailsDat.";
 
-
-
-export default function  Eduction(){
+export default function Eduction() {
     return (
-        
+        <div className="col-8">
+            <h4>&lt;h2&gt;</h4>
+            {detailsDat.eduction.map(element => {
+                return (
+                    <div className="row col-12">
+                        <h6 className="col-11 offset-1">&lt;h2&gt;
+                        {element.title.toUpperCase()}
+                        &lt;/h2&gt;
+                        </h6>
+                       <p className="col-11 offset-1">&lt;p&gt;{element.description} &lt;/p&gt;</p>
+                    </div>
+                )
+            })}
+            <h2>&lt;/h2&gt;</h2>
+        </div>
+
     )
 }
